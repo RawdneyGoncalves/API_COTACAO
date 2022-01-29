a13f61daea74d9865db82d4035f20bd9e56f1a88
RawdneyMendes <53352051+RawdneyGoncalves@users.noreply.github.com>
2022-01-29 06:11:56 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+const request = require("request");
+
+//https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL
+
+const money = "USD-BRL,EUR-BRL,BTC-BRL";
+const options = {
+  url: `https://economia.awesomeapi.com.br/last/${money}`,
+  headers: {
+    Accept: "application/json",
+    "Accept-Charset": "utf-8",
+  },
+};
+//DOLAR
+const callback_dolar = function (error, response, body) {
+  try {
+    let json = JSON.parse(body);
+    cotacao = json.USDBRL["bid"];
+    dia = json.USDBRL["create_date"];
+    console.log("DOLAR = R$: " + cotacao + " Dia: " + dia);
+  } catch (error) {
+    console.log(error);
+  }
+};
+//REAL
+const callback_dolar = function (error, response, body) {
+  try {
+    let json = JSON.parse(body);
+    cotacao = json.USDBRL["bid"];
+    dia = json.USDBRL["create_date"];
+    console.log("DOLAR = R$: " + cotacao + " Dia: " + dia);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+request(options, callback_dolar);
